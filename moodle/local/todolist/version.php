a4afd90378e22404a8d96feabe6f39220572cb5b
Mike McGowan <mike.mcgowan.75@gmail.com>
2017-03-19 14:12:54 +0000

    Added plugin dependencies

--- moodle/local/todolist/version.php
+++ moodle/local/todolist/version.php
@@ -2,6 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017030200;
-$plugin->requires  = 2016112900;
-$plugin->component = 'local_todolist';
+$plugin->version      = 2017030200;
+$plugin->requires     = 2016112900;
+$plugin->component    = 'local_todolist';
+$plugin->dependencies = [
+    'theme_plus' => 2017031600,
+];
--- moodle/theme/plus/version.php
+++ moodle/theme/plus/version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2017031600';
-$plugin->requires = '2016120502';
-$plugin->component = 'theme_plus';
+$plugin->version      = 2017031600;
+$plugin->requires     = 2016120502;
+$plugin->component    = 'theme_plus';
 $plugin->dependencies = [
-    'theme_boost' => '2016120500',
+    'theme_boost' => 2016120500,
 ];
