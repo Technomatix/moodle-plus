f213b484db20162badd1e5cf5d95e54b23fbe65a
Mike McGowan <mike.mcgowan.75@gmail.com>
2017-03-21 15:27:16 +0000

    Fixed a bug in the utils/uninit.sh script

--- utils/uninit.sh
+++ utils/uninit.sh
@@ -5,9 +5,9 @@ docker-compose rm -f
 
 rm -f composer.lock
 rm -rf vendor
-rm -rf moodle/vendor
 rm -rf moodledata
-rm -f config.php
+rm -rf moodle/vendor
+rm -f moodle/config.php
 
 docker volume rm moodleplus_pgdata
 docker network rm moodleplus_default
